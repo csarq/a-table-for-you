774ee285fea9f5dc2e2db6fbb6221f81ba2fc822
csarq <catherinesarquis@gmail.com>
2024-06-23 17:29:49 +0100

    basic homepage structure

---
+++ inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>A Table for You by Chef José</title>
+</head>
+<body>
+  <main>
+    <h1 class="title">A Table for You by Chef José</h1>
+    <nav class="nav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Sample Menus</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+      </nav> 
+
+  
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include 'inc/header.php';
+?>
+        <section class="quote">
+            <blockquote>
+                Embark on a culinary adventure, designed exclusively for you.
+            </blockquote>
+        </section>
+  <section class="events-list">
+<h2>Elevate Your Occasions</h2>
+<ul>
+<li>Exclusive Private Dining</li>
+<li>Celebratory Galas &amp; Milestones</li>
+<li>Corporate Hospitality &amp; Retreats</li>
+<li>Intimate Gatherings &amp; Soirées</li>
+<li>Curated Culinary Experiences</li>
+</ul>
+<p>And any other occasion where discerning tastes demand the finest culinary artistry.</p>
+</section>
+  <img class="image1" src="img/tuna tataki.png" alt="Seared tuna tataki">
+  <img class="image2" src="img/raspberry tart.jpg" alt="dark chocolate raspberry tart">
+  </main>
+  </body>
+</html>
