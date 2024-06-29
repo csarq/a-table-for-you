be518b62e720a06a1a8eec8d08188193c5d60716
csarq <catherinesarquis@gmail.com>
2024-06-29 22:37:59 +0100

    adds padding to about text

--- about.php
+++ about.php
@@ -7,9 +7,9 @@ include 'inc/sitegrid-nav.inc.php';
 
 <section class="about">
     <h2 class="noto-sans-h2">About A Table for You</h2>
-    <p>Whether you're celebrating a cherished family milestone, hosting esteemed business associates, or seeking a discreet dining experience, A Table For You caters to your every desire.</p>
+    <p>Whether you're celebrating a cherished family milestone, hosting esteemed business associates, or seeking a discreet dining experience, <em>A Table For You</em> caters to your every desire.</p>
 
 <p>Chef José, acclaimed for his tenure in Michelin-starred kitchens and his unwavering commitment to culinary excellence, creates bespoke menus tailored to your unique preferences and occasion.</p>
 
-<p>From intimate family gatherings to high-level business dinners, every detail is meticulously curated to ensure an unforgettable experience. Indulge in the finest seasonal ingredients, expertly prepared dishes, and impeccable service, all within the comfort and privacy of your chosen setting. Discover the true meaning of personalized dining with A Table For You.</p>
+<p>From intimate family gatherings to high-level business dinners, every detail is meticulously curated to ensure an unforgettable experience. Indulge in the finest seasonal ingredients, expertly prepared dishes, and impeccable service, all within the comfort and privacy of your chosen setting. Discover the true meaning of personalised dining with A Table For You.</p>
 </section>
